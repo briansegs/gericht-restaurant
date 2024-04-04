30d260c123d359ed915e46f3a34b00fb928a4d19
bsegers <briansegs@gmail.com>
2024-04-04 10:39:46 -0400

    add: show on scroll and resize

--- src/components/ReturnToTop/ReturnToTop.jsx
+++ src/components/ReturnToTop/ReturnToTop.jsx
@@ -1,14 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { BsArrowUpSquare } from 'react-icons/bs';
 
 import './ReturnToTop.css';
 
-const ReturnToTop = () => (
+const ArrowBtn = () => (
     <div className='app__returntotop'>
-        <BsArrowUpSquare 
-            className='app__returntotop_arrow-icon' 
-        />
+        <a href="#home">
+            <BsArrowUpSquare 
+                className='app__returntotop_arrow-icon' 
+            />
+        </a>  
     </div>
-);
+)
+
+const ReturnToTop = () => {
+    const [scrollPosition, setScrollPosition] = useState(0);
+    
+    const handleScroll = () => {
+        const position = window.scrollY;
+        setScrollPosition(position);    
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, { passive: true });
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const [widthPosition, setWidthPosition] = useState(0);
+
+    const handleResize = () => {
+        const width = window.innerWidth;
+        setWidthPosition(width);    
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize, { passive: true });
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return (
+        <>
+        {(scrollPosition > 150 && 
+            widthPosition > 1150) && (
+                <ArrowBtn />
+        )}
+
+        {(scrollPosition > 1000 && 
+            widthPosition < 1150 && 
+            widthPosition > 900) && (
+                <ArrowBtn />
+        )}
+        </>
+    )
+}
 
 export default ReturnToTop
